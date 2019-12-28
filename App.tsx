af8c5b736bf86a2864effd5ead7cecff2b7e2e82
Łukasz M <mierzejewski94@gmail.com>
2019-12-28 09:12:29 +0000

    MTG-3 Add Native Base

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import React, {Fragment, useEffect} from 'react';
-import SplashScreen from 'react-native-splash-screen';
-
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
+import {Text} from 'native-base';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
   useEffect(() => {
     setTimeout(() => SplashScreen.hide(), 1000);
   });
 
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
 }
